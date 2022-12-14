c5d9583bde2fdd7949aee31f4f7c70f84c15c1da
RaghavsScarletSplendour <72269666+RaghavsScarletSplendour@users.noreply.github.com>
2022-12-14 11:53:17 +0530

    V 0.1
    Added Component File
    Added Header.js file and basic words

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Heading = () => {
+    return(
+        <div>
+            <h5>About Projects Achievements Resume</h5>
+        </div>
+    )
+}
+
+export default Heading
